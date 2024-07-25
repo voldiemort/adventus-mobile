d31cff1390d69154fbb910e8be7d4d088bb0ecf4
Xan <iamxanng@gmail.com>
2024-07-26 00:38:10 +0800

    login page frontend added

--- app/login.tsx
+++ app/login.tsx
@@ -1,10 +1,11 @@
+import LoginForm from '@/components/auth/LoginForm';
 import { Text, View } from 'react-native';
 
 export default function LoginPage() {
   return (
     <View className='flex-1 flex-col items-center justify-center'>
       <Text className='absolute top-[35px] left-[20px] text-3xl font-bold'>Adventus</Text>
-      <Text>This is the login page</Text>
+      <LoginForm/>
     </View>
   ) 
 }
--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,21 +1,76 @@
-import { useForm, SubmitHandler } from "react-hook-form";
+import { View, TextInput, Alert } from "react-native"
+import { Button } from "../ui/button"
+import { Text } from "../ui/text"
+import { Input } from "../ui/input"
+import { Label } from "../ui/label"
+import { useForm, Controller } from "react-hook-form"
+import React from "react"
 
-type Inputs = {
-    email: string,
-    password: string
-};
 
-const LoginForm = () => {
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+export default function App() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
+  const onSubmit = handleSubmit((data) => console.log(data));
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-        </form>
-    )
+
+  return (
+    <View className="w-3/4 gap-4">
+      <Controller
+        control={control}
+        rules={{
+          required: 'Required',
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+            <View>
+                <Label nativeID="email">Email</Label>
+                <Input
+                    placeholder="Your email"
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    aria-labelledby="email"
+                    className='border-2 rounded-lg my-[5px]'
+                />
+                {errors.email && <Text className="text-destructive">{errors.email.message}</Text>}
+            </View>
+        )}
+        name="email"
+      />
+
+      <Controller
+        control={control}
+        rules={{
+            required: 'Required',
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+            <View>
+                <Label nativeID="password">Password</Label>
+                <Input
+                    placeholder="Your password"
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    aria-labelledby="password"
+                    className='border-2 rounded-lg my-[5px]'
+                    secureTextEntry={true}
+                />
+                {errors.password && <Text className="text-destructive">{errors.password.message}</Text>}
+            </View>
+        )}
+        name="password"
+      />
+
+        <Button onPress={onSubmit}>
+            <Text>Sign in with email</Text>
+        </Button>
+    </View>
+  )
 }
